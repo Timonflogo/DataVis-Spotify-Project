d7f85ba9278908f56ad91149f31d8baeff46aa36
Jonas Ringive Korsholm <Jonas.k345@gmail.com>
2021-10-12 16:41:12 +0200

    Adding solution to arrange track features in a data frame

--- get_track_features.R
+++ get_track_features.R
@@ -1,4 +1,15 @@
-library(dplyr)
+## Libraries
+pacman::p_load("jsonlite"
+               , "dplyr"
+               , "tidyr"
+               , "spotifyr"
+               , "pbapply"
+)
+
+Sys.setenv(SPOTIFY_CLIENT_ID = 'a0299cfb25944ecdbffc9079b987ff9a')
+Sys.setenv(SPOTIFY_CLIENT_SECRET = 'f773ae310efa4f74a751bb6f50d4b0b4')
+access_token <- get_spotify_access_token()
+
 #loading the data
 artist_id <- readRDS(file = 'artist_id.RDS')
 df_unique_artist_name <- readRDS(file = 'df_unique_artist_name.RDS')
@@ -9,7 +20,8 @@ joined_df <- artist_id %>%
   left_join(df_unique_artist_name
             , by = 'track_artist') %>% 
   left_join(df_unique_track_id
-            , by = 'track_artist')
+            , by = 'track_artist') %>% 
+  head(50)
 
 # function for extracting all features of each song in the list based on the id
 f <- function(track_feature){
@@ -25,14 +37,26 @@ f <- function(track_feature){
 df_track_features <- data.frame()
 
 # Running the function and saving it into the dataframe
-system.time({df_track_features <- pbsapply(X = joined_df$track_id,FUN = f) %>% t()}, gcFirst = TRUE)
+system.time({df_track_features <- pbsapply(X = joined_df$track_id,FUN = f)}, gcFirst = TRUE)
 
-# df_track_feature <- data.frame()
-# for (i in seq(from = 1, to = 10)) {
-#   df_temp <- data.frame()
-#   df_temp <- df_track_features[1, i][[1]]
-#   df_track_feature <- rbind(df_track_feature, df_temp)
-# }
+df <- do.call(rbind,df_track_features)
+
+
+df_track_feature <- data.frame()
+for (i in seq(from = 1, to = length(df_track_features))) {
+  
+  #Print current entry
+  print(i)
+  
+  #Control if the track has 18 features
+  if(length(df_track_features[[i]]) != 18) {
+    print(paste0("skip track: ",i))
+    next
+  }
+  df_track_feature <- rbind(df_track_feature,df_track_features[[i]])
+}
+
+df_track_features <- df_track_feature
 
 # Rename the column id to track_id for easier join
 df_track_features <- df_track_features %>% 
@@ -44,8 +68,8 @@ main_df <- joined_df %>%
   left_join(df_track_features
             , by = 'track_id')
 
-# saveRDS(df_track_features, file = "get_track_features.RDS")
-# test <- readRDS(file = 'get_track_features.RDS')
+# saveRDS(df_track_features, file = "get_track_features.rds")
+# test <- readRDS(file = 'get_track_features.rds')
 
 
 
