79b5966a46938a9c2dd5bddd15bab1b80b9fbcbd
hieptomas <71631947+hieptomas@users.noreply.github.com>
2021-10-15 22:41:47 +0200

    Stream ready for vis
    selected the first artist of each row
    cleaned the artist_id column from unnecessary characters
    features are rescaled into range of 0 -1
    calculated features per ms to have 1 single unit for aggregation
    column endTime splitted into date and time

--- R files/3_stream_w_features.R
+++ R files/3_stream_w_features.R
@@ -4,6 +4,8 @@ pacman::p_load("jsonlite"
                , "tidyr"
                , "spotifyr"
                , "pbapply"
+               , "stringr"
+               , "data.table"
 )
 
 Sys.setenv(SPOTIFY_CLIENT_ID = 'a0299cfb25944ecdbffc9079b987ff9a')
@@ -37,4 +39,52 @@ track_features <- readRDS('get_track_features.RDS')
 stream_full_features <- df %>% 
   inner_join(track_features, by = "track_artist")
 
+# splitting artist_id into first id and the rest 
+temp_artist <- str_split(string = stream_full_features$artist_id
+                         , pattern = ","
+                         , n = 2
+                         , simplify = TRUE) %>% #splitting artist_id into 2 columns - main artist and the rest
+  as.data.frame() %>% 
+  select(1) %>% # selecting the first artist
+  rename('artist_id' = "V1") %>% # Renaming the column back to artist_id
+  unlist() %>% # unlist in order to use str_replace 
+  # Cleaning the artist_id from 'c' and brackets
+  str_replace(pattern = 'c\\("' 
+              , replacement = "") %>% 
+  str_replace(pattern = '"'
+              , replacement = "") %>% 
+  as.data.frame() %>% # Setting it back into a dataframe
+  rename('artist_id' = ".") #renaming it back to artist_id
+
+# Replacing artist_id in stream_full_features with temp_artist
+stream_full_features <- stream_full_features %>% 
+  mutate(artist_id = temp_artist$artist_id)
+
+# Selecting columns to work with since some are irrelevant
+stream_selected_c <- stream_full_features %>% 
+  select(artist_id
+         , track_id
+         , msPlayed
+         , endTime
+         , duration_ms
+         , 9, 10, 12, 14:19, ) %>% 
+  # standardizing features into range of 0 - 1
+  mutate_at(scales::rescale
+            , .vars = vars(6:14))
+
+# Calculating the features per ms played to have a single unit for aggregation
+stream_selected_c <- setDT(stream_selected_c)[ , paste0(names(stream_selected_c)[6:14]
+                                                        , "_per_ms") := lapply(.SD,`/`, stream_selected_c$msPlayed)
+                                               , .SDcols = danceability : tempo]
+
+# Creating date and time columns from endTime
+stream_selected_c <- stream_selected_c %>% 
+  mutate(date = as.Date(endTime)
+         , time = format(as.POSIXct(endTime
+                                    , format = "%Y-%m-%d %H:%M")
+                         , "%H:%M:%S")) %>% 
+  relocate(c(date, time) #change the position of these 2 columns 
+           , .after = endTime)
+
+# saveRDS(stream_selected_c, file = "stream_selected_c_clean.rds")
 
