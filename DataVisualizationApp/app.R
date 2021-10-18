318269270e251f9acc973a47c4fecc13ca10ac80
Jonas Ringive Korsholm <Jonas.k345@gmail.com>
2021-10-18 11:28:53 +0200

    Streamgraph script

---
+++ DataVisualizationApp/app.R
@@ -0,0 +1,70 @@
+# Libraries
+library(shiny)
+library(shinythemes)
+library(shinyWidgets)
+library(plotly)
+library(tidyverse)
+
+
+# Define UI for application that draws a histogram ----
+# Define UI for application that draws a histogram
+ui <- navbarPage(
+    theme = shinytheme("cerulean"),
+    fluid = TRUE, 
+    "Movie recommender system",
+    
+    #' *Tab 1*
+    tabPanel(title = "Welcome",
+             h3("Spotify of making movie recommendations"),
+             p("This is some text introduction"),
+    ),
+    
+    
+    #' *Tab 2: Streaming history* 
+    tabPanel(title = "Streaming history statistics",
+             h3("Spotify streaming history"),
+             p("This is your streaming history."),
+             
+             #Create fluid row to set inputs side-by-side
+             fluidRow(
+                 column(width = 2,
+                        sliderInput(inputId = "weeks",label = "Select weeks:"
+                                    ,min = 1,max = 52,value = 1
+                                    ,width = 700),
+                 )
+             ),
+             fluidPage(
+                 # column(width = 2,
+                 #        numericInput(inputId = "no_recc_euc",label =  "Euc. dist recommendations:"
+                 #                     ,value =  5,min = 1,max = 10),
+                 # ),
+                 # 
+                 # 
+                 # column(width = 2,
+                 #        numericInput(inputId = "no_recc_lda",label =  "Topic recommendations:"
+                 #                     ,value =  5,min = 1,max = 10),
+                 # ),
+                 # 
+                 mainPanel(
+                     tableOutput(outputId = "distPlot"),
+                     width = 11
+                 )
+             )
+    )
+)
+
+# Define server logic required to draw a histogram ----
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$weeks + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ Streamgraph.R
@@ -0,0 +1,22 @@
+# libraries
+require("remotes")
+remotes::install_github("hrbrmstr/streamgraph")
+pacman::p_load(reshape2,ggplot2,ggstream,plotly,streamgraph,RColorBrewer)
+
+
+## Streamgraph data ----
+df <- readRDS("R dataframe/stream_selected_c_clean.rds")
+
+stream_df <- df %>% 
+  melt(1:10) %>%  #Keep columns 1 - 9, create a row entry for each column value of 10 - 18
+  group_by(start_day_week,variable) %>% 
+  summarise(value = sum(value))
+
+
+## Plotting ----
+# plot <- ggplot(stream_df, aes(x = week_number, y = value, fill = variable)) +
+#   geom_stream() +
+#   geom_stream_label(aes(label = variable))
+
+streamgraph::streamgraph(stream_df, key="variable", value="value", date="start_day_week", height="600px", width="1800px") %>% 
+  sg_fill_brewer("Spectral") #BrBG PiYG PRGn PuOr RdBu RdGy RdYlBu RdYlGn Spectral
