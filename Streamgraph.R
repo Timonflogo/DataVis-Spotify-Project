baa6577aaf2fa1b7600c0dbe9e128dd5d9b05d61
Jonas Ringive Korsholm <Jonas.k345@gmail.com>
2021-10-18 17:55:27 +0200

    Coloring stream chart

--- Streamgraph.R
+++ Streamgraph.R
@@ -27,19 +27,18 @@ cap_space <- function(string){capitalize(str_replace(string,pattern = "_",replac
 idx <- seq(from = 1,to = length(stream_gg$index),by = 9 * 4.45) #months are on avg. 4.5 weeks
 
 ggplot(stream_gg, aes(x = index, y = value, fill = cap_space(string = variable))) +
-  geom_stream(n_grid = 57,bw = 0.25) + #bw = wigglyness
-  #geom_stream_label(aes(label = cap_space(string = variable))) + 
+  geom_stream(n_grid = 57,bw = 0.25) +
+  ggtitle("Streaming history features") + 
   theme(panel.background = element_blank()
         ,panel.grid.major.x = element_line(size = 0.3, color="black",linetype = "dotted")
         ,axis.text.x = element_text(angle = 0)
         ,axis.ticks = element_blank()
+        ,plot.title = element_text(hjust = 0.5)
         ) +
   scale_x_continuous(labels = stream_gg$month[idx]
                      ,breaks = stream_gg$index[idx]
                      ,n.breaks = length(stream_gg$month[idx])) +
-  labs(fill = 'Features',x = "Time") -> p
-
-plot(p)
+  labs(fill = 'Features',x = "Time")
 
 # streamgraph::streamgraph(stream_df, key="variable", value="value", date="start_day_week", height="600px", width="1800px") %>% 
 #   #sg_fill_brewer("Spectral") %>% #BrBG PiYG PRGn PuOr RdBu RdGy RdYlBu RdYlGn Spectral
