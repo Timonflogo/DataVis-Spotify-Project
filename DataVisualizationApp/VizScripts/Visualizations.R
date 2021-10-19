4fcdb626ee51fd34622247c67f2c1a4897bab543
Jonas Ringive Korsholm <Jonas.k345@gmail.com>
2021-10-19 15:27:43 +0200

    update

--- DataVisualizationApp/VizScripts/Visualizations.R
+++ DataVisualizationApp/VizScripts/Visualizations.R
@@ -1,63 +1,78 @@
 ## Streamgraph ----
 streamgraph <- function(dataInput1){
-source("ConvenienceFunctions/ConvenienceFunctions.R")
-
-#Load aggregated features
-stream_gg <- dataInput1 %>% 
-  melt(1:11) %>%  #Keep columns 1 - 9, create a row entry for each column value of 10 - 18
-  group_by(year_week,month,variable) %>% 
-  summarise(value = sum(value))
-stream_gg$index <- sort(rep(seq(1,55),9))
-
-idx <- seq(from = 1,to = length(stream_gg$index),by = 9 * 4.45) #months are on avg. 4.5 weeks
-
-  ggplot(stream_gg, aes(x = index, y = value, fill = cap_space(string = variable))) +
-    geom_stream(n_grid = 57,bw = 0.5) + #bw = wigglyness
+  #source("ConvenienceFunctions/ConvenienceFunctions.R")
+  idx <- seq(from = 1,to = length(stream_group_date$index),by = 9 * (367 / 12)) #months are on average 30.5 days
+  
+  ggplot(stream_group_date, aes(x = index, y = value, fill = cap_space(string = variable))) +
+    geom_stream(n_grid = 375,bw = 0.50) + #bw = wigglyness
     scale_fill_manual(values = RColorBrewer::brewer.pal(n = 9,name = 'Blues')) +
     theme(panel.background = element_blank()
           ,panel.grid.major.x = element_line(size = 0.3, color="black",linetype = "dotted")
           ,axis.text.x = element_text(angle = 0)
           ,axis.ticks = element_blank()
     ) +
-    scale_x_continuous(labels = stream_gg$month[idx]
-                       ,breaks = stream_gg$index[idx]
-                       ,n.breaks = length(stream_gg$month[idx])) +
+    scale_x_continuous(labels = stream_group_date$month[idx]
+                       ,breaks = stream_group_date$index[idx]
+                       ,n.breaks = length(stream_group_date$month[idx])) +
     labs(fill = 'Features',x = "Time")
 }
 
-#streamgraph(dataInput = stream_gg)
+
 
 ## Ribbon chart ---- 
 ribbonchart <- function(dataInput1){
-stream_group_weekday <- dataInput1 %>% 
-  melt(1:11) %>% 
-  group_by(weekday, variable) %>% 
-  summarise(value = sum(value)) %>% 
-  mutate(weekday = ordered(weekday, levels=c("Monday", "Tuesday", "Wednesday", "Thursday", 
-                                             "Friday", "Saturday", "Sunday")))
-
-library(ggalluvial)
-ggplot(data = stream_group_weekday,
-       aes(x = weekday
-           , y = value
-           , alluvium = variable)) +
-  scale_fill_manual(values = RColorBrewer::brewer.pal(n = 9
-                                                      ,name = 'Blues')) +
-  geom_alluvium(aes(fill = cap_space(string = variable))
-                , alpha = .75
-                , decreasing = FALSE
-                , linetype = 0) +
-  theme(panel.background = element_blank()
-        , panel.grid.major.x = element_line(size = 0.3
-                                            , color="black"
-                                            ,linetype = "dotted")
-        , axis.ticks = element_blank()) +
-  labs(fill = 'Features',x = "Time")
+  stream_group_weekday <- dataInput1 %>% 
+    melt(1:11) %>% 
+    group_by(weekday, variable) %>% 
+    summarise(value = sum(value)) %>% 
+    mutate(weekday = ordered(weekday, levels=c("Monday", "Tuesday", "Wednesday", "Thursday", 
+                                               "Friday", "Saturday", "Sunday")))
+  
+  ggplot(data = stream_group_weekday,
+         aes(x = weekday
+             , y = value
+             , alluvium = variable)) +
+    scale_fill_manual(values = RColorBrewer::brewer.pal(n = 9
+                                                        ,name = 'Blues')) +
+    geom_alluvium(aes(fill = cap_space(string = variable))
+                  , alpha = .75
+                  , decreasing = FALSE
+                  , linetype = 0) +
+    theme(panel.background = element_blank()
+          , panel.grid.major.x = element_line(size = 0.3
+                                              , color="black"
+                                              ,linetype = "dotted")
+          , axis.ticks = element_blank()) +
+    labs(fill = 'Features',x = "Time")
 }
 
 
+## Radar chart ----
 
-
-
-
-
+radarplot <- function(dataInput1){
+  
+    radar_gg <- dataInput1 %>%
+      melt(1:11) %>%
+      group_by(variable) %>%
+      summarise(value = mean(value)) %>% 
+      # melt(1:4) %>%  #Keep columns 1 - 9, create a row entry for each column value of 10 - 18
+      # group_by( variable) %>%
+      # summarise(value = mean(value)) %>%
+      spread(variable, value)
+    
+    rownames(radar_gg) <- "exposure"
+    radar_gg <- rbind(rep(1,ncol(radar_gg)) , rep(0,ncol(radar_gg)) , radar_gg)
+    
+    colors_border=c( rgb(0.2,0.5,0.5,0.9), rgb(0.8,0.2,0.5,0.9) , rgb(0.7,0.5,0.1,0.9) )
+    colors_in=c( rgb(0.2,0.5,0.5,0.4), rgb(0.8,0.2,0.5,0.4) , rgb(0.7,0.5,0.1,0.4) )
+    
+    # plot with default options:
+    radarchart( radar_gg, axistype=1 ,vlabels = cap_space(colnames(radar_gg)),
+                #custom polygon
+                pcol=colors_border , pfcol=colors_in , plwd=4 , plty=1,
+                #custom the grid
+                cglcol = "grey", cglty=1, axislabcol= "grey",caxislabels=seq(0,1,0.25), cglwd=0.8, #
+                #custom labels
+                vlcex=0.8
+    )
+}
--- DataVisualizationApp/app.R
+++ DataVisualizationApp/app.R
@@ -1,17 +1,16 @@
 # Libraries
-# require("remotes")
-# remotes::install_github("hrbrmstr/streamgraph")
-pacman::p_load(reshape2,ggplot2,ggstream,streamgraph,RColorBrewer,Hmisc,dplyr,gridExtra,shiny, shinythemes, shinyWidgets, plotly, tidyverse,ggalluvial)
+pacman::p_load(reshape2,ggplot2,ggstream,streamgraph,RColorBrewer,Hmisc,dplyr,gridExtra,shiny,shinythemes
+               ,shinyWidgets,plotly,tidyverse,ggalluvial,fmsb)
 
 # Commands to run on initiating the app
 df <- readRDS("Data/stream_selected_c_clean.rds") #Shortened, if run manually instert: DataVisualizationApp/
 source("ConvenienceFunctions/ConvenienceFunctions.R")
 source("VizScripts/Visualizations.R")
 
-#Load aggregated features
-stream_group_date <- df %>% 
-    melt(1:11) %>% 
-    group_by(date, month, variable) %>% 
+#THIS IS ONLY FOR STREAMHISTORY BRUSHING
+stream_group_date <- df %>%
+    melt(1:11) %>%
+    group_by(date, month, variable) %>%
     summarise(value = sum(value))
 stream_group_date$index <- sort(rep(seq(1,nrow(stream_group_date)/length(unique(stream_group_date$variable))),9)) #Create an index to map the y values onto a continous x axis
 
@@ -37,10 +36,11 @@ ui <- navbarPage(
                  
                  plotOutput(outputId = "Ribbonchart"),
                  
+                 plotOutput(outputId = "Radarchart"),
+                 
                  textOutput(outputId = "FilterText"),
                  
                  dataTableOutput(outputId = "BrushedData"),
-                 dataTableOutput(outputId = "BrushedData1"),
                  
                  width = 11
              )
@@ -52,10 +52,17 @@ server <- function(input,output){
     
     ## Getting data ----
     
+    #THIS IS ONLY FOR STREAMHISTORY BRUSHING
+    stream_group_date <- df %>%
+        melt(1:11) %>%
+        group_by(date, month, variable) %>%
+        summarise(value = sum(value))
+    stream_group_date$index <- sort(rep(seq(1,nrow(stream_group_date)/length(unique(stream_group_date$variable))),9)) #Create an index to map the y values onto a continous x axis
+
     ### masterData ----
     masterData <- reactive({
         
-        df <- readRDS("Data/stream_selected_c_clean.rds") #Shortened, if run manually instert: DataVisualizationApp/
+        df <- readRDS("Data/stream_selected_c_clean.rds")
         data <- brushedPoints(df = stream_group_date,brush = input$plot1_brush)
         if (nrow(data) == 0)
             data <- stream_group_date
@@ -68,67 +75,36 @@ server <- function(input,output){
         
     })
     
-    ### stream_weekday ----
-    stream_weekday <- reactive({
-        
-        stream_group_weekday <- masterData() %>% 
-            melt(1:11) %>% 
-            filter(date >= filter_start_date & date <= filter_end_date) %>% 
-            group_by(weekday, variable) %>% 
-            summarise(value = sum(value))
-        
-        #Return the data
-        stream_group_weekday
-    })
-    
-    
     ### Filter Text ----
     output$FilterText <- renderText({
-        
         masterData() #To activate the master data function
-        
         paste0("You have filtered your data from ",filter_start_date," to ",filter_end_date)
     })
-    
     
     ## Viz ----
     
     ### Streamgraph ----
     output$Streamgraph <- renderPlot({
-        
-        idx <- seq(from = 1,to = length(stream_group_date$index),by = 9 * (367 / 12)) #months are on average 30.5 days
-        
-        ggplot(stream_group_date, aes(x = index, y = value, fill = cap_space(string = variable))) +
-            geom_stream(n_grid = 375,bw = 0.50) + #bw = wigglyness
-            scale_fill_manual(values = RColorBrewer::brewer.pal(n = 9,name = 'Blues')) +
-            theme(panel.background = element_blank()
-                  ,panel.grid.major.x = element_line(size = 0.3, color="black",linetype = "dotted")
-                  ,axis.text.x = element_text(angle = 0)
-                  ,axis.ticks = element_blank()
-            ) +
-            scale_x_continuous(labels = stream_group_date$month[idx]
-                               ,breaks = stream_group_date$index[idx]
-                               ,n.breaks = length(stream_group_date$month[idx])) +
-            labs(fill = 'Features',x = "Time")
+        streamgraph(dataInput1 = stream_group_date)
     }
     )
     
     ### Ribbon chart ----
     
     output$Ribbonchart <- renderPlot({
-        
         ribbonchart(dataInput1 = masterData())
-        
+    })
+    
+    ### Radar chart ----
+    
+    output$Radarchart <- renderPlot({
+        radarplot(dataInput1 = masterData())
     })
     
     ## Test ----
     
     output$BrushedData <- renderDataTable({
         masterData()
-    })
-    
-    output$BrushedData1 <- renderDataTable({
-        stream_weekday()
     })
     
 }
