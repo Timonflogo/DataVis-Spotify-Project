b1b337a932d367507ba0f246d75fe596ec9e7280
hieptomas <71631947+hieptomas@users.noreply.github.com>
2021-11-02 12:44:41 +0100

    Update 6_summary_stats.R

--- R files/6_summary_stats.R
+++ R files/6_summary_stats.R
@@ -108,3 +108,17 @@ favourite_track <- d %>%
   mutate(total_time_played = round(total_time_played,0)) %>% 
   arrange(desc(total_time_played)) %>% 
   top_n(1)
+
+
+
+
+# Year - the release year of the recording. Note that due to vagaries of releases, re-releases, re-issues and general madness, sometimes the release years are not what you'd expect.
+# Energy - The energy of a song - the higher the value, the more energtic song
+# Danceability - The higher the value, the easier it is to dance to this song.
+# Loudness (dB) - The higher the value, the louder the song.
+# Liveness - The higher the value, the more likely the song is a live recording.
+# Valence - The higher the value, the more positive mood for the song.
+# Length - The duration of the song.
+# Acousticness - The higher the value the more acoustic the song is.
+# Speechiness - The higher the value the more spoken word the song contains.
+# Duration - The length of the song.
