3020d61934ca91bfc5a2c76949986f1247cefa8d
hieptomas <71631947+hieptomas@users.noreply.github.com>
2021-11-02 13:32:37 +0100

    summary_stats_functions

--- DataVisualizationApp/VizScripts/Summary_stats.R
+++ DataVisualizationApp/VizScripts/Summary_stats.R
@@ -1,27 +1,69 @@
-d <- readRDS('DataVisualizationApp/Data/stream_selected_c_clean.rds')
+d <- readRDS('Data/stream_selected_c_clean.rds')
 library(dplyr)
 library(lubridate)
 library(hms)
 
+total_time_played <- function(dataInput1){
+  round(seconds_to_period(sum(d$dataInput1) / 1000), 0)
+}
+# total_time_played <- round(seconds_to_period(sum(d$msPlayed) / 1000), 0)
 
-# time_played_per_month <- d %>%
-#   group_by(month) %>%
-#   summarise(total_time_played = seconds_to_period(sum(msPlayed) / 1000)) %>%
-#   mutate(total_time_played = round(total_time_played, 0)) %>%
-#   arrange(total_time_played)
-# 
-# favourite_artist <- d %>%
-#   group_by(artistName) %>%
-#   summarise(total_time_played = seconds_to_period(sum(msPlayed) / 1000)) %>%
-#   mutate(total_time_played = round(total_time_played, 0)) %>%
-#   top_n(1)
-# 
-# time_played_per_weekdays <- d %>%
-#   group_by(weekday) %>%
-#   summarise(total_time_played = seconds_to_period(sum(msPlayed) / 1000)) %>%
-#   mutate(total_time_played = round(total_time_played, 0)) %>%
-#   arrange(desc(total_time_played))
-# 
+total_songs_played <- function(dataInput1){
+  nrow(dataInput1)
+}
+# total_songs_played <- nrow(d)
+
+total_unique_songs_played <- function(dataInput1){
+  dataInput1 %>% 
+    group_by(artistName
+             , trackName) %>% 
+    summarise(freq = n()) %>% 
+    ungroup() %>% 
+    nrow()
+}
+# total_unique_songs_played <- d %>% 
+#   group_by(artistName
+#            , trackName) %>% 
+#   summarise(freq = n()) %>% 
+#   ungroup() %>% 
+#   nrow()
+
+total_unique_artist_played <- function(dataInput1){
+  dataInput1 %>% 
+    group_by(artistName
+             , trackName) %>% 
+    summarise(freq = n()) %>% 
+    select(artistName) %>% 
+    unique() %>% 
+    nrow()
+}
+# total_unique_artist_played <- d %>% 
+#   group_by(artistName
+#            , trackName) %>% 
+#   summarise(freq = n()) %>% 
+#   select(artistName) %>% 
+#   unique() %>% 
+#   nrow()
+
+favourite_artist <- function(dataInput1){
+  temp <- dataInput1 %>%
+    group_by(artistName) %>%
+    summarise(freq_played = n()
+              , total_time_played = seconds_to_period(sum(msPlayed) / 1000)) %>%
+    mutate(total_time_played = round(total_time_played,0)) %>% 
+    arrange(desc(freq_played)) %>%
+    top_n(1)
+  artist_name <- temp$artistName
+  freq_played <- temp$freq_played
+  total_time_played <- temp$total_time_played
+  print(paste('Your favourite artist '
+              , artist_name
+              , 'was played '
+              , freq_played
+              , 'times and in total for'
+              , total_time_played))
+}
+
 # favourite_artist <- d %>%
 #   group_by(artistName) %>%
 #   summarise(freq_played = n()
@@ -29,7 +71,30 @@ library(hms)
 #   mutate(total_time_played = round(total_time_played,0)) %>% 
 #   arrange(desc(freq_played)) %>%
 #   top_n(1)
-# 
+
+favourite_track <- function(dataInput1){
+  temp <- dataInput1 %>%
+    group_by(trackName, artistName) %>%
+    summarise(freq_played = n()
+              , total_time_played = seconds_to_period(sum(msPlayed) / 1000)) %>%
+    ungroup() %>% 
+    mutate(total_time_played = round(total_time_played,0)) %>% 
+    arrange(desc(total_time_played)) %>% 
+    top_n(1)
+  trackName <- temp$trackName
+  artistName <- temp$artistName
+  freq_played <- temp$freq_played
+  total_songs_played <- temp$total_time_played
+  print(paste('Your most listned track is '
+              , trackName
+              , 'from '
+              , artistName
+              , 'and it was listened for'
+              , freq_played
+              , 'times equivalently to '
+              , total_time_played))
+}
+
 # favourite_track <- d %>%
 #   group_by(trackName, artistName) %>%
 #   summarise(freq_played = n()
@@ -38,76 +103,6 @@ library(hms)
 #   mutate(total_time_played = round(total_time_played,0)) %>% 
 #   arrange(desc(total_time_played)) %>% 
 #   top_n(1)
-# 
-# freq_per_track <- d %>%
-#   group_by(trackName, artistName) %>%
-#   summarise(freq_played = n()
-#             , total_time_played = seconds_to_period(sum(msPlayed) / 1000)) %>%
-#   arrange(desc(total_time_played))
-# 
-# total_time_played_per_year <- d %>% 
-#   mutate(year = as.numeric(format(date,'%Y'))) %>% 
-#   group_by(year) %>% 
-#   summarise(total_time_played = seconds_to_period(sum(msPlayed) / 1000)) %>% 
-#   arrange(desc(total_time_played))
-# 
-# freq_tracks_played_per_year <- d %>% 
-#   mutate(year = as.numeric(format(date,'%Y'))) %>% 
-#   group_by(year) %>% 
-#   summarise(freq_played = n()
-#             , total_time_played = seconds_to_period(sum(msPlayed) / 1000))
-# 
-# features_per_weekday <- d %>% 
-#   group_by(weekday) %>% 
-#   summarise(danceability = sum(danceability_exposed)
-#             , energy = sum(energy_exposed)
-#             , loudness = sum(loudness_exposed)
-#             , speechiness = sum(speechiness_exposed)
-#             , acousticness = sum(acousticness_exposed)
-#             , instrumentalness = sum(instrumentalness_exposed)
-#             , liveness = sum(liveness_exposed)
-#             , valence = sum(valence_exposed)
-#             , tempo = sum(tempo_exposed)) %>% 
-#   arrange(desc(danceability))
-
-total_time_played <- round(seconds_to_period(sum(d$msPlayed) / 1000), 0)
-  # group_by(month) %>% 
-  # summarise(total_time_played = round(seconds_to_period(sum(msPlayed) / 1000), 0)) %>% 
-  # filter(month == lubridate::month(Sys.Date(), label = TRUE, abbr = FALSE)) 
-
-total_songs_played <- nrow(d)
-
-total_unique_songs_played <- d %>% 
-  group_by(artistName
-           , trackName) %>% 
-  summarise(freq = n()) %>% 
-  ungroup() %>% 
-  nrow()
-
-total_unique_artist_played <- d %>% 
-  group_by(artistName
-           , trackName) %>% 
-  summarise(freq = n()) %>% 
-  select(artistName) %>% 
-  unique() %>% 
-  nrow()
-
-favourite_artist <- d %>%
-  group_by(artistName) %>%
-  summarise(freq_played = n()
-            , total_time_played = seconds_to_period(sum(msPlayed) / 1000)) %>%
-  mutate(total_time_played = round(total_time_played,0)) %>% 
-  arrange(desc(freq_played)) %>%
-  top_n(1)
-
-favourite_track <- d %>%
-  group_by(trackName, artistName) %>%
-  summarise(freq_played = n()
-            , total_time_played = seconds_to_period(sum(msPlayed) / 1000)) %>%
-  ungroup() %>% 
-  mutate(total_time_played = round(total_time_played,0)) %>% 
-  arrange(desc(total_time_played)) %>% 
-  top_n(1)
 
 
 
