c88df416eb03feb245f4605519d906141e499e3e
TimonFloGo <timon.godt@live.com>
2021-11-09 10:29:47 +0100

    Revert "added convenience functoin for radar chart data"
    This reverts commit c34573d7facf4594c423ae6a468a0452d44ce001.

--- DataVisualizationApp/VizScripts/Visualizations.R
+++ DataVisualizationApp/VizScripts/Visualizations.R
@@ -60,39 +60,17 @@ linechart <- function(dataInput1){
 
 
 ## Radar chart ----
-getSeason <- function(DATES) {
-  WS <- as.Date("2021-12-21", format = "%Y-%m-%d") # Winter Solstice
-  SE <- as.Date("2021-3-20",  format = "%Y-%m-%d") # Spring Equinox
-  SS <- as.Date("2021-6-21",  format = "%Y-%m-%d") # Summer Solstice
-  FE <- as.Date("2021-9-22",  format = "%Y-%m-%d") # Fall Equinox
-  
-  # Convert dates from any year to 2012 dates
-  d <- as.Date(strftime(DATES, format="2021-%m-%d"))
-  
-  ifelse (d >= WS | d < SE, "Winter",
-          ifelse (d >= SE & d < SS, "Spring",
-                  ifelse (d >= SS & d < FE, "Summer", "Autmn")))
-}
 
 radarplot <- function(dataInput1){
   
   radar_gg <- dataInput1 %>%
-    mutate(season = as.factor(getSeason(date))) %>%
-    select(1, 2:6, 42, 7:41) %>% 
-    melt(1:14) %>%  # melt(which(names(dataInput1) == "track_id"):which(names(dataInput1) == "duration_ms")) %>% dont use this, its not working
-    group_by(season, variable) %>%
+    melt(1:13) %>%  # melt(which(names(dataInput1) == "track_id"):which(names(dataInput1) == "duration_ms")) %>% dont use this, its not working
+    group_by(variable) %>%
     summarise(value = mean(value)) %>% 
     spread(variable, value) %>% 
-    select(20:28) %>% 
-    mutate(season = as.character(season))
+    select(20:28)
   
-    # radar_gg <- rename(sub(colnames(radar_gg), pattern = "_exposed", replacement = ""))
-  
-  radar_gg <- radar_gg %>% 
-    remove_rownames %>% 
-    column_to_rownames(var="season") %>% 
-    as.data.frame()
-  
+  rownames(radar_gg) <- "exposure"
   radar_gg <- rbind(rep(1,ncol(radar_gg)) , rep(0,ncol(radar_gg)) , radar_gg)
   
   colors_border=c( rgb(0.2,0.5,0.5,0.9), rgb(0.8,0.2,0.5,0.9) , rgb(0.7,0.5,0.1,0.9) )
@@ -100,7 +78,7 @@ radarplot <- function(dataInput1){
   
   # plot with default options:
   par(bg = '#E6E6E6')
-  radarchart( radar_gg, axistype=1,vlabels = cap_space_radar(colnames(radar_gg)),
+  radarchart( radar_gg, axistype=1 ,vlabels = cap_space(colnames(radar_gg)),
               #custom polygon
               pcol=colors_border , pfcol=colors_in , plwd=4 , plty=1,
               #custom the grid
@@ -108,8 +86,6 @@ radarplot <- function(dataInput1){
               #custom labels
               vlcex=0.8
   )
-  
-  legend(x=.9, y=1.3, legend = rownames(radar_gg[-c(1,2),]), bty = "n", pch=20 , col=colors_in , text.col = "grey", cex=1.2, pt.cex=3)
 }
 
 ## Barchart ----
@@ -163,51 +139,3 @@ barchart <- function(dataInput1,current_artist = current_artist()){
     layout(plot_bgcolor='#E6E6E6') %>% 
     layout(paper_bgcolor='#E6E6E6')
 }
-
-### Horizanchart ----
-
-horizonchart <- function(dataInput1, NumArtists) {
-  
-  target = as.list(df_barchart[1])
-  
-  horizon_data <- dataInput1 %>% 
-    select(time, artistName, msPlayed) %>% 
-    filter(artistName %in% unlist(target)) %>% 
-    mutate(time = sub(pattern = ":", replacement = "" , time)) %>% 
-    mutate(time = sub(pattern = ":00", replacement = "" , time)) %>% 
-    mutate(time = as.numeric(time)) %>% 
-    mutate(time1 = ifelse(nchar(time) > 3, 
-                          yes =  as.numeric(substring(time, first = 1, last = 2)),
-                          no = as.numeric(substring(time, first = 1, last = 1)))) %>% 
-    mutate(time1 = time1 * 60) %>% 
-    mutate(time2 = as.numeric(substring(time, first = 3, last = 4))) %>% 
-    mutate(time_minutes = time1 + time2)
-  
-  horizon_data %>%  
-    ggplot() +
-    geom_horizon(aes(time_minutes/60, 
-                     msPlayed), origin = 'min', horizonscale = 4) +
-    facet_wrap(~artistName, ncol = 1, strip.position = 'right') +
-    scale_fill_hcl(palette = 'Peach', reverse = T) +
-    facet_grid(artistName~.) +
-    theme_few() +
-    theme(
-      plot.background = element_rect(fill = "#E6E6E6", colour = "#E6E6E6"),
-      panel.background = element_rect(fill = "#E6E6E6", colour = "#E6E6E6"),
-      panel.spacing.y=unit(0, "lines"),
-      strip.text.y = element_text(size = 7, angle = 0, hjust = 0),
-      legend.position = 'none',
-      axis.text.y = element_blank(),
-      axis.title.y = element_blank(),
-      axis.ticks.y = element_blank(),
-      panel.border = element_blank(),
-      legend.background = element_rect(fill = "#E6E6E6")
-    ) +
-    scale_x_continuous(
-      name = 'Time',
-      breaks=seq(from = 3, to = 27, by = 3),
-      labels = function(x) {sprintf("%02d:00", as.integer(x %% 24))}) +
-    ggtitle('Time of day the Top 10 artists are played')
-  
-}
-
