d629b769d77e3673b700c32b3d332d689223a894
TimonFloGo <timon.godt@live.com>
2021-11-23 10:21:11 +0100

    changed Radarchart from Base R to Plotly

--- DataVisualizationApp/VizScripts/mod_radarchart.R
+++ DataVisualizationApp/VizScripts/mod_radarchart.R
@@ -30,28 +30,78 @@ radarplot <- function(dataInput1){
     select(21:28) %>% 
     mutate(season = as.character(season))
   
-  radar_gg <- radar_gg %>% 
-    remove_rownames %>% 
-    column_to_rownames(var="season") %>% 
-    as.data.frame()
+  radar_gg <- radar_gg %>%
+    remove_rownames %>%
+    column_to_rownames(var="season") %>%
+    as.data.frame() %>% 
+    round(digits = 2)
   
-  radar_gg <- rbind(rep(1,ncol(radar_gg)) , rep(0,ncol(radar_gg)) , radar_gg)
+
   
-  colors_border=c( rgb(0.2,0.5,0.5,0.9), rgb(0.3,0.6,0.1,0.9) , rgb(0.7,0.5,0.1,0.9), rgb(0.8,0.2,0.5,0.9) )
-  colors_in=c( rgb(0.2,0.5,0.3,0.1), rgb(0.3,0.6,0.1,0.1), rgb(0.7,0.5,0.3,0.1), rgb(0.8,0.2,0.5,0.1) )
+  fig <- plot_ly(
+    type = 'scatterpolar',
+    fill = 'toself'
+  ) 
+  fig <- fig %>%
+    add_trace(
+      r = as.numeric(radar_gg[1,]),
+      theta = cap_space_radar(colnames(radar_gg)),
+      name = 'Autmn'
+    ) 
+  fig <- fig %>%
+    add_trace(
+      r = as.numeric(radar_gg[4,]),
+      theta = cap_space_radar(colnames(radar_gg)),
+      name = 'Winter'
+    ) 
+  fig <- fig %>%
+    add_trace(
+      r = as.numeric(radar_gg[2,]),
+      theta = cap_space_radar(colnames(radar_gg)),
+      name = 'Spring'
+    ) 
+  fig <- fig %>%
+    add_trace(
+      r = as.numeric(radar_gg[3,]),
+      theta = cap_space_radar(colnames(radar_gg)),
+      name = 'Summer'
+    ) 
+  fig <- fig %>%
+    layout(
+      title = "Average Music exposure",
+      font = 1, 
+      polar = list(
+        radialaxis = list(
+          visible = T,
+          range = c(0,1)
+          )),
+      plot_bgcolor  = "rgb(230, 230, 230)", 
+      paper_bgcolor = "rgb(230, 230, 230)",
+      fig_bgcolor   = "rgb(230, 230, 230)",
+      autosize = F, height = 500, margin = list(l=50, r=50, b=50, t=50, pad=1)
+    )
   
-  # plot with default options:
-  par(bg = '#E6E6E6', mar = c(1,1,0.1, 0.1) )
-  radarchart( radar_gg, axistype=1,vlabels = cap_space_radar(colnames(radar_gg)),
-              #custom polygon
-              pcol=colors_border , pfcol=colors_in , plwd=2 , plty=1,
-              #custom the grid
-              cglcol = "grey", cglty=1, axislabcol= "grey",caxislabels=seq(0,1,0.25), cglwd=0.8, #
-              #custom labels
-              vlcex=0.8,
-              # change opacity
-              
-  )
+
   
-  legend(x=.85, y=1.3, legend = rownames(radar_gg[-c(1,2),]), bty = "n", pch=20 , col=colors_border , text.col = "black", cex=1.2, pt.cex=3)
+  fig
+  
+  # radar_gg <- rbind(rep(1,ncol(radar_gg)) , rep(0,ncol(radar_gg)) , radar_gg)
+  # 
+  # colors_border=c( rgb(0.2,0.5,0.5,0.9), rgb(0.3,0.6,0.1,0.9) , rgb(0.7,0.5,0.1,0.9), rgb(0.8,0.2,0.5,0.9) )
+  # colors_in=c( rgb(0.2,0.5,0.3,0.1), rgb(0.3,0.6,0.1,0.1), rgb(0.7,0.5,0.3,0.1), rgb(0.8,0.2,0.5,0.1) )
+  # 
+  # # plot with default options:
+  # par(bg = '#E6E6E6', mar = c(1,1,0.1, 0.1) )
+  # radarchart( radar_gg, axistype=1,vlabels = cap_space_radar(colnames(radar_gg)),
+  #             #custom polygon
+  #             pcol=colors_border , pfcol=colors_in , plwd=2 , plty=1,
+  #             #custom the grid
+  #             cglcol = "grey", cglty=1, axislabcol= "grey",caxislabels=seq(0,1,0.25), cglwd=0.8, #
+  #             #custom labels
+  #             vlcex=0.8,
+  #             # change opacity
+  #             
+  # )
+  # 
+  # legend(x=.85, y=1.3, legend = rownames(radar_gg[-c(1,2),]), bty = "n", pch=20 , col=colors_border , text.col = "black", cex=1.2, pt.cex=3)
 }
